2ede327e9d7e562dabf4a7f04e3c574f07f8fb2e
Atsushi Takayama <taka.atsushi@gmail.com>
2014-03-16 12:21:22 +0900

    add command to start iig

---
+++ bin/iig.rb
@@ -0,0 +1,12 @@
+#!/usr/bin/env ruby
+
+Net::IRC::Server.new('localhost', ENV['IIG_PORT'] || 6667, IdobataIrcGateway, {
+  redis: {
+    host: '127.0.0.1',
+    port: 6379,
+    key: 'hubot'
+  },
+  irc: {
+    main_channel: ENV['IIG_MAIN_CHANNEL']
+  }
+}).start
